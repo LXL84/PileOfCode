cdf466a64fd859abe8f89bdfc49c01f400ab3a9c
LXL84 <LXL84@users.noreply.github.com>
2018-12-08 17:29:27 +0800

    Update Pile1.py

--- Pile1.py
+++ Pile1.py
@@ -57,3 +57,30 @@ def strfdelta(tdelta, fmt):
 print(strfdelta((datetime.now() - startTime), "{hours}:{minutes}:{seconds}s")+' to complete the prediction')
 #1 days 20:18:12
 
+http://sphweb.bumc.bu.edu/otlt/MPH-Modules/BS/BS704_HypothesisTesting-ChiSquare/BS704_HypothesisTesting-ChiSquare_print.html 
+# chi-squared test with similar proportions
+from scipy.stats import chi2_contingency
+from scipy.stats import chi2
+# contingency table
+table = [	[10, 20, 30],
+			[6,  9,  17]]
+print(table)
+print(table)
+stat, p, dof, expected = chi2_contingency(table)
+print('dof=%d' % dof)
+print(expected)
+# interpret test-statistic
+prob = 0.95
+critical = chi2.ppf(prob, dof)
+print('probability=%.3f, critical=%.3f, stat=%.3f' % (prob, critical, stat))
+if abs(stat) >= critical:
+	print('Dependent (reject H0)')
+else:
+	print('Independent (fail to reject H0)')
+# interpret p-value
+alpha = 1.0 - prob
+print('significance=%.3f, p=%.3f' % (alpha, p))
+if p <= alpha:
+	print('Dependent (reject H0)')
+else:
+	print('Independent (fail to reject H0)')
